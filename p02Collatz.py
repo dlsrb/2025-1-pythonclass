f0b89f8cf9caf328c2550ccecd3c816a86f6199d
user <dlsrb1997@naver.com>
2025-04-08 11:51:34 +0900

    콜라츠 추측 코딩

--- p02Collatz.py
+++ p02Collatz.py
@@ -5,16 +5,42 @@
 #     n이 홀수 -> 3*n+1
 #예: 5-> 16 -> 8 -> 4 -> 2 -> 1  (5단계)
 #
-n = 99
+from itertools import count
 
-# 단계의 갯수를 셀것
+# 단계의 갯수를 셀것 - done
 # n을 1부터 99까지 변화하면서, 각각의 단계 수를 출력할 것
 # 그중 가장 큰 수를 찾을 것
 # n = 97: 118번만에 1로 도달
+# n = 73: 115번만에 1로 도달
 
-while n!=1:
-    if n % 2 == 1:
-        n = 3 * n + 1
-    else:
-        n = n / 2
-    print(n)
+maxvalue = 0
+maxvaluen = 0
+max2value = 0
+max2valuen = 0
+
+for n in range(1,100):
+    # print(f'{n=}')
+    ncount = 0
+    i = n # n과 i를 따로둬 n은 초기값만 존재
+    #nsave = n #n 값을 바꾸기전에 초기값
+
+    while i!=1:
+        if i % 2 == 1:
+            i = 3 * i + 1
+        else:
+            i = i / 2
+        ncount = ncount + 1
+
+
+
+    print(f'{ncount}')
+    if maxvalue < ncount:
+        max2value = maxvalue
+        max2valuen = maxvaluen
+        maxvalue = ncount
+        maxvaluen = n
+    elif ncount > max2value:
+        max2value = ncount
+        max2valuen = n
+print(f'{maxvalue=}, {maxvaluen=}')
+print(f'{max2value=}, {max2valuen}')
